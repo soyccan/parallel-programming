0f36474cc4fddcc0fe42049f7e5e0cc337add494
soyccan <soyccan@gmail.com>
2022-01-31 21:27:45 +0800

    improve time complexity

--- 10084 Prefix Sum-pthread/prefixsum-pthread.c
+++ 10084 Prefix Sum-pthread/prefixsum-pthread.c
@@ -11,6 +11,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <pthread.h>
 
@@ -45,40 +46,39 @@
 #define MAX_THREAD 6
 
 struct argument {
-  uint32_t *prefix;
-  int start_idx;
+  uint32_t *arr;
+  int start_idx; // index in original array
   int len;
-  union {
-    uint32_t val;
-    uint32_t key;
-  };
+  uint32_t val; // key / value to add
 };
 
+// calculate prefix sum of each subinterval
 void *calc_prefix_sub(void *arg_) {
   struct argument *arg = arg_;
   uint32_t sum = 0;
   log("calc_prefix_sub start_idx=%d len=%d key=%u", arg->start_idx, arg->len,
-      arg->key);
-  for (int i = 0; i < arg->len; i++) {
-    sum += encrypt(arg->start_idx + i, arg->key);
-    arg->prefix[i] = sum;
-  }
-  return NULL;
-}
-
-void *merge_prefix(void *arg_) {
-  struct argument *arg = arg_;
-  log("merge_prefix start_idx=%d len=%d val=%u", arg->start_idx, arg->len,
       arg->val);
   for (int i = 0; i < arg->len; i++) {
-    arg->prefix[i] += arg->val;
+    sum += encrypt(arg->start_idx + i, arg->val);
+    arg->arr[i] = sum;
   }
   return NULL;
 }
 
-uint32_t prefix_sum[MAXN];
+// add each element in arr[0..len] by val
+void *array_range_add(void *arg_) {
+  struct argument *arg = arg_;
+  log("array_range_add start_idx=%d len=%d val=%u", arg->start_idx, arg->len,
+      arg->val);
+  for (int i = 0; i < arg->len; i++) {
+    arg->arr[i] += arg->val;
+  }
+  return NULL;
+}
 
-// put arguments at global is faster
+// putting data shared by threads at global is faster
+uint32_t prefix_sum[MAXN];
+uint32_t prefix_sum_sub[MAX_THREAD]; // prefix sum of subinterval sum
 struct argument arguments[MAX_THREAD];
 
 int main() {
@@ -107,33 +107,60 @@ int main() {
         num_thread, remain_size);
 
     // calculate prefix sum of each subinterval
+    // O(n/p)
     for (int i = 0; i < num_thread; i++) {
       int start_idx = 1 + block_size * i;
-      arguments[i].prefix = prefix_sum + start_idx;
+      arguments[i].arr = &prefix_sum[start_idx];
       arguments[i].start_idx = start_idx;
       arguments[i].len = block_size - (i == num_thread - 1) * remain_size;
-      arguments[i].key = key;
+      arguments[i].val = key;
+    }
+    for (int i = 0; i < num_thread; i++) {
       pthread_create(&pool[i], NULL, calc_prefix_sub, &arguments[i]);
     }
     for (int i = 0; i < num_thread; i++) {
       pthread_join(pool[i], NULL);
     }
 
-    // merge subinterval prefix in tree structure
+    // calculate prefix sum of subinterval sum
+    // O(log p)
+    for (int i = 0; i < num_thread; i++) {
+      prefix_sum_sub[i] = prefix_sum[1 + block_size * (i + 1) - 1];
+    }
     for (int step = 1; step < MAX_THREAD; step *= 2) {
       int num_thread_2 = num_thread - step;
       for (int i = 0; i < num_thread_2; i++) {
         // get all val before launching threads to prevent race condition
-        int val_idx = 1 + block_size * (i + 1) - 1;
-        arguments[i + step].val = prefix_sum[val_idx];
+        arguments[i].arr = &prefix_sum_sub[i + step];
+        arguments[i].start_idx = i + step;
+        arguments[i].len = 1;
+        arguments[i].val = prefix_sum_sub[i];
       }
       for (int i = 0; i < num_thread_2; i++) {
-        pthread_create(&pool[i], NULL, merge_prefix, &arguments[i + step]);
+        pthread_create(&pool[i], NULL, array_range_add, &arguments[i]);
       }
       for (int i = 0; i < num_thread_2; i++) {
         pthread_join(pool[i], NULL);
       }
       log("");
+    }
+
+    // add prefix sum of subinterval sum to get prefix sum of original array
+    // O(n/p)
+    int num_thread_2 = num_thread - 1;
+    for (int i = 0; i < num_thread_2; i++) {
+      // get all val before launching threads to prevent race condition
+      int start_idx = 1 + block_size * (i + 1);
+      arguments[i].arr = &prefix_sum[start_idx];
+      arguments[i].start_idx = start_idx;
+      arguments[i].len = block_size - (i == num_thread_2 - 1) * remain_size;
+      arguments[i].val = prefix_sum_sub[i];
+    }
+    for (int i = 0; i < num_thread_2; i++) {
+      pthread_create(&pool[i], NULL, array_range_add, &arguments[i]);
+    }
+    for (int i = 0; i < num_thread_2; i++) {
+      pthread_join(pool[i], NULL);
     }
 
     output(prefix_sum, n);
